0a1f6dde3015787a7d16d23901c4e0d2c0a3afd2
Andrew Smith <boosmith@gmail.com>
2019-09-08 14:53:57 +0100

    chore: Add Material-ui package

---
+++ build/precache-manifest.4992068b12c9658fab0e2b46537fd161.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5c3f15620da06138e104f4c4d3439b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d9403fde466dff14d44",
+    "url": "/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "444dbe527b3ce32d378e",
+    "url": "/static/js/2.68301339.chunk.js"
+  },
+  {
+    "revision": "9d9403fde466dff14d44",
+    "url": "/static/js/main.98d43dde.chunk.js"
+  },
+  {
+    "revision": "ef50aa0d2dc6d64fa3da",
+    "url": "/static/js/runtime~main.f2897aef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
